031e678e43f85ab104e9463fd5397c35240b82ca
unknown <bhaveshgarud18@gmail.com>
2024-11-09 21:59:18 +0530

    video

--- src/components/VideoEditingPortfolio.tsx
+++ src/components/VideoEditingPortfolio.tsx
@@ -26,7 +26,7 @@ const VideoEditingPortfolio = () => {
     {
       title: "Music Video",
       description: "Vibrant music video for an up-and-coming artist",
-      videoUrl: "https://www.youtube.com/watch?v=UtzDqzfZQLQ&ab_channel=RukmaniGurukul",
+      videoUrl: "https://youtu.be/UtzDqzfZQLQ?si=4SE0kLV409H1JfkQ",
       tags: ["Visual Effects", "Editing"]
     }
   ]
