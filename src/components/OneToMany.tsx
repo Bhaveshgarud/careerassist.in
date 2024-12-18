fa10e673f919e8776e2752b21cd02b90f22a4b8d
Bhavesh Garud <bharatchandra949@gmail.com>
2024-12-18 16:32:10 +0530

    prices

--- src/components/OneToMany.tsx
+++ src/components/OneToMany.tsx
@@ -100,7 +100,7 @@ function OneToMany() {
                   </li>
                   <li className="flex items-center">
                     <CheckCircleIcon className="h-5 w-5 text-green-500 mr-2" />
-                    AutoApply job: 200 Applications
+                    AutoApply job: 100 Applications
                   </li>
                 </ul>
               </CardContent>
@@ -120,7 +120,7 @@ function OneToMany() {
                   </li>
                   <li className="flex items-center">
                     <CheckCircleIcon className="h-5 w-5 text-green-500 mr-2" />
-                    AutoApply job: 100 Applications
+                    AutoApply job: 50 Applications
                   </li>
                 </ul>
               </CardContent>
